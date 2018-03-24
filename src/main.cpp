bfdcf72e5b97313988b2e9f52a1b837f841dfe25
Szymon <sz.wawrzyczek@gmail.com>
2018-03-24 14:50:03 +0100

    avl tree height

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ public:
     Node<T>* Getroot(){return root;};
     void setRoot(Node<T>*tmp){root=tmp;};
     void display(Node<T>*,int);
+    int tree_height(Node<T> *tmp);
 };
 
 template <class T>
@@ -46,23 +47,21 @@ Node<T>* AVL_tree<T>::insert(Node<T> *parent,T value)
 	if(parent==NULL) 
 	{
 		parent=new Node<T> (value);
-        cout<<"parent = NULL"<<endl;
 		return parent;
 	}
     else if(value < parent->GiveValue())
     {
         tmp=insert(parent->Getleft(),value);
         parent->setLeft(tmp);
-        cout<<"new left child"<<endl;
     }
     else if(value >= parent->GiveValue())
     {
         tmp=insert(parent->Getright(),value);
         parent->setRight(tmp);
-        cout<<"new right child"<<endl;
     }
 return parent;
 }
+
 template <class T>
 void AVL_tree<T>::display(Node<T> *parent,int level)
 {
@@ -84,6 +83,20 @@ void AVL_tree<T>::display(Node<T> *parent,int level)
     }
 }
 
+template <class T>
+int AVL_tree<T>::tree_height(Node<T> *tmp)
+{
+    int height=0;
+    if (tmp!=NULL)
+    {
+        int left_height=tree_height(tmp->Getleft());
+        int right_height=tree_height(tmp->Getright());
+        int max_height=max(left_height,right_height);
+        height=max_height+1;
+    }
+    return height-1;
+}
+
 /******************************************************************************/
 int main()
 {   
@@ -91,14 +104,15 @@ int main()
     float wartosc;
     Node<float> *tmp_root=NULL;
     AVL_tree <float> tree;
-    for(int i=0; i<15; i++)
+    for(int i=0; i<9; i++)      //testowanie
     {
-        wartosc=rand()%3;
+        wartosc=rand()%51;          
         tmp_root=tree.insert(tree.Getroot(),wartosc);
         tree.setRoot(tmp_root);
     }
 
         tree.display(tree.Getroot(),1);
+        cout<<"wysokosc avl tree= "<<tree.tree_height(tree.Getroot())<<endl;
 
 
 
